5cbcc92a0d755b8e3c4ff9872060ec84cf1ffc5c
aprdhita <nadaforinumaki@gmail.com>
2024-09-04 13:02:22 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors :{
+        hr : '#000000',
+        title :'#A46049',
+      },
+    },
+  },
+  plugins: [],
+}
+
